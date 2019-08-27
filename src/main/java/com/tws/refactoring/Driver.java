4d25f92c8f2bd85e0394cff49e4caf399c6942c0
wangqiming1001 <512821390@qq.com>
2019-08-27 11:16:06 +0800

    chonggou

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,9 +1,19 @@
 package com.tws.refactoring;
 
 public class Driver {
-    public int age;
+ //   public int age;
 
-    public Driver(int age) {
+//    public Driver(int age) {
+//        this.age = age;
+//    }
+    private int age;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Driver(int age) {
         this.age = age;
     }
 }
--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,17 @@
 package com.tws.refactoring;
 
 public class Police {
+//    public boolean checkDriver(Driver driver) {
+//        if(driver.age >= 18) return true;
+//        else return false;
+//    }
+    private static int AGE = 18;
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
-        else return false;
-    }
+    	int temp = driver.getAge();
+    	if (temp>= AGE) {
+    		return true;
+		}else {
+			return true;
+		}
+    }  
 }
